1d0bac379fb4531235a698e3cd496b3add1c7d49
Micki-D <michael.dudkowiak@tum.de>
2021-11-21 15:00:48 +0100

    cleaned up code a bit

--- src/Tobs.jl
+++ src/Tobs.jl
@@ -1,23 +1,11 @@
 # This file is a part of RunStatistics.jl, licensed under the MIT License (MIT).
 
+#Think about implementing a special case with μ and σ as vectors, so each obsevation X[i] can have an individual normal distribution
+
 function Tobs(X::AbstractArray, μ::Real, σ2::Real)
     χ2 = []
     χi = 0
     for i in range(1, length(X))
-        
-        X[i] > μ ?  χi += (X[i] - μ)^2 / σ2 : (append!(χ2, χi) ; χi = 0)
-
-        #if X[i] > μ
-        #    χi += (X[i] - μ)^2 / σ2
-        #else
-        #    append!(χ2, χi)
-       #     χi = 0
-       # end 
-    end
- 
+        X[i] > μ ?  χi += (X[i] - μ)^2 / σ2 : (append!(χ2, χi); χi = 0)
     return maximum(χ2)
-
 end
- 
-
-println(Tobs([-1, 1, 3, -2], 0, 1))
