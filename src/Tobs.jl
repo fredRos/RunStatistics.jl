4576cd21e6231880db3c14f98fae83c214319427
Micki-D <michael.dudkowiak@tum.de>
2021-11-25 14:45:37 +0100

    added implementation of partitioning n into k parts, needs to be tied into squares to work properly.

--- src/Tobs.jl
+++ src/Tobs.jl
@@ -7,7 +7,6 @@ function Tobs(X::AbstractArray, μ::Real, σ2::Real)
     χi = 0
     for i in range(1, length(X))
         X[i] > μ ?  χi += (X[i] - μ)^2 / σ2 : (append!(χ2, χi); χi = 0)
+    end 
     return maximum(χ2)
 end
-
-println(Tobs([-1, 1, 3, -2], 0, 1))
