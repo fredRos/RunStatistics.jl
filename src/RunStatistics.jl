143e609fbdbf240e88a28ea1f7ba938fb7de04de
Micki-D <michael.dudkowiak@tum.de>
2021-12-23 14:56:47 +0100

    a

--- src/RunStatistics.jl
+++ src/RunStatistics.jl
@@ -1,10 +1,5 @@
 # This file is a part of RunStatistics.jl, licensed under the MIT License (MIT).
 
-"""
-    RunStatistics
-
-Template for Julia packages.
-"""
 module RunStatistics
 include("squares.jl")
 include("squares_approx.jl")
