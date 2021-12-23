56300737056a9794af2f06712ba200b99a9af1dd
Micki-D <michael.dudkowiak@tum.de>
2021-12-23 19:10:57 +0100

    i dont know anymore

--- src/RunStatistics.jl
+++ src/RunStatistics.jl
@@ -1,7 +1,7 @@
 # This file is a part of RunStatistics.jl, licensed under the MIT License (MIT).
 
 module RunStatistics
-include("squares.jl")
+include("partitions.jl")
 include("squares_approx.jl")
 include("partitions.jl")
 include("tobs.jl")
