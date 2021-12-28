be6c99d0a3d3ec854ba40ac42507b5cdb1a0b1ac
Micki-D <michael.dudkowiak@tum.de>
2021-12-28 11:07:08 +0100

    bre

--- src/RunStatistics.jl
+++ src/RunStatistics.jl
@@ -1,12 +1,6 @@
 # This file is a part of RunStatistics.jl, licensed under the MIT License (MIT).
 
 module RunStatistics
-<<<<<<< HEAD
-=======
-include("partitions.jl")
-include("squares.jl")
-include("squares_approx.jl")
->>>>>>> f05a976e597092b29d0a698d66171600c82a86fc
 include("partitions.jl")
 include("squares_approx.jl")
 include("squares.jl")
