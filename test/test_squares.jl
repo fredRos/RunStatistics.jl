4539324b9124336236ce78894c89edead6974487
kourosh <krshz247@outlook.com>
2022-01-01 19:05:40 +0100

    Fix: added test for pvalye() and cumulative().

--- test/test_squares.jl
+++ test/test_squares.jl
@@ -5,7 +5,8 @@ using RunStatistics
 using Test
 
 @testset "squares" begin
-    @test 1 == 1
+    @test pvalue(3.4, 30) == 0.818208032939994
+    @test cumulative(3.4, 30) == 0.18179196706000597
 end
 
 # test agains the c++ version 
