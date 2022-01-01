ed4ec1e46b1bfd8b0aabdf78ca1bc73fa19b8176
kourosh <krshz247@outlook.com>
2022-01-01 19:13:45 +0100

    Fix: added test for squares_approx.jl and squares.jl .

--- test/test_squares.jl
+++ test/test_squares.jl
@@ -4,9 +4,12 @@
 using RunStatistics
 using Test
 
+Tobs = 3.4
+N = 30
+
 @testset "squares" begin
-    @test pvalue(3.4, 30) == 0.818208032939994
-    @test cumulative(3.4, 30) == 0.18179196706000597
+    @test pvalue(Tobs, N) == 0.818208032939994
+    @test cumulative(Tobs, N) == 0.18179196706000597
 end
 
 # test agains the c++ version 
--- test/test_squares_approx.jl
+++ test/test_squares_approx.jl
@@ -4,7 +4,15 @@
 using RunStatistics
 using Test
 
+Tobs = 20
+N = 30
+n = 100
+epsrel = 0.1
+epsabs = 0.001
+
+
 @testset "squares_approx" begin
-    @test 1 == 1
+    @test approx_pvalue(Tobs, N, n, epsrel, epsabs) == 0.241642
+    @test approx_cumulative(Tobs, N, n, epsrel, epsabs) == 0.758358
 end
 
