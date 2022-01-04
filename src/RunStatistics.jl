c890d92d1f1ad68088f4be1b2b27118b14087708
Kourosh Zargari <krshz247@outlook.com>
2022-01-04 09:55:33 +0100

    Feature: module and export
    set Runstatistics as precompile, and enabled pvalue and cumulative to be exported.

--- src/RunStatistics.jl
+++ src/RunStatistics.jl
@@ -1,6 +1,10 @@
 # This file is a part of RunStatistics.jl, licensed under the MIT License (MIT).
+__precompile__()
 
 module RunStatistics
+
+export pvalue, cumulative
+
 include("tobs.jl")
 include("partitions.jl")
 include("squares.jl")
