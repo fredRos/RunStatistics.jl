9244039c3446e44fd89bf7cd4f02224b63f3f8e2
Kourosh Zargari <krshz247@outlook.com>
2022-01-11 18:33:14 +0100

    Refactor: t_obs refactoring * added argcheck for dimension consistency * for loop structure * type stabilization of the inputs

--- src/t_obs.jl
+++ src/t_obs.jl
@@ -13,15 +13,14 @@ For the Squares statistic to be calculable, the observed data must satisfy follo
         Mean µ_i and variance σ^2_i are known.
 """
 function t_obs(X::AbstractArray, μ::Real, σ2::Real)
+
     T = float(promote_type(eltype(X), eltype(μ), eltype(σ2)))
     χ2 = T[]
     χi = zero(T)
-    for i in eachindex(X)
-        X[i] > μ ? χi += (X[i] - μ)^2 / σ2 : (append!(χ2, χi);
-        χi = 0)
+
+    @inbounds for i in eachindex(X)
+        X[i] > μ ? χi += (X[i] - μ)^2 / σ2 : (append!(χ2, χi); χi = 0)
     end
-
-
 
     return maximum(χ2)
 end
@@ -35,14 +34,10 @@ function t_obs(X::AbstractArray, μ::AbstractArray, σ2::AbstractArray)
     χi = zero(T)
 
     @inbounds for i in eachindex(X)
-        X[i] > μ[i] ? χi += (X[i] - μ[i])^2 / σ2[i] : (append!(χ2, χi);
-        χi = 0)
+        X[i] > μ[i] ? χi += (X[i] - μ[i])^2 / σ2[i] : (append!(χ2, χi); χi = 0)
     end
 
     return maximum(χ2)
 
     
 end
-
-
-
